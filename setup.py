3db6cfe668e23ec00aa5fdf835b0858a2e0fc93a
Alex Zvoleff <azvoleff@gmail.com>
2017-05-31 16:44:28 -0400

    Fix packages list.

--- setup.py
+++ setup.py
@@ -62,7 +62,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['landdegradation', 'docs', 'tests']),
+    packages=find_packages(exclude=['docs', 'tests']),
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
