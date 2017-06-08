5d6e67ffa45d26e9dcc32279070147abbc929ac4
Alex Zvoleff <azvoleff@gmail.com>
2017-06-08 18:23:49 -0400

    Install ee from git.

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn', 'ee'],
+    install_requires=['peppercorn', 'https://github.com/google/earthengine-api.git#egg=ee&subdirectory=python'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
