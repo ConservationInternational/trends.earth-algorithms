33fe524042516d483e5d8e45d107c3ad192f8c71
Alex Zvoleff <azvoleff@gmail.com>
2017-06-27 10:47:03 -0400

    Require marshmallow.

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['peppercorn', 'earthengine-api>=0.1.114'],
+    install_requires=['peppercorn', 'marshmallow', 'earthengine-api>=0.1.114'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
