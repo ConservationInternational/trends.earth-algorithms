fba225a35f616794bef32ccc27738256e86329d1
Alex Zvoleff <azvoleff@gmail.com>
2017-12-02 14:31:12 -0500

    Bump version.

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1a1',
+    version='0.2',
 
     description='A python package to facilitate analyzing remotely-sensed datasets in support of monitoring land degradation status.',
     long_description=long_description,
