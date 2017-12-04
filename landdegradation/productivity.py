9feca0614865cffa11e5f4b57e6d303752c87dd4
Alex Zvoleff <azvoleff@gmail.com>
2017-12-04 12:02:50 -0500

    Fix nodata coding.
    Mariano uses -9999 for nodata in assets datasets, but we want to save these as 9999 for outputs for use in the toolbox.

--- landdegradation/productivity.py
+++ landdegradation/productivity.py
@@ -174,7 +174,7 @@ def productivity_trajectory(year_start, year_end, method, ndvi_gee_dataset,
     # -3 is degraded (pvalue < 0.01), 3 is improving (pvalue < 0.1), 2 is 
     # improving (pvalue < 0.05), 3 is improving (pvalue < 0.01)
     attri = ee.Image(0) \
-        .where(lf_trend.select('scale').lte(9999), 9999) \
+        .where(lf_trend.select('scale').lte(-9999), 9999) \
         .where(lf_trend.select('scale').gt(0).And(mk_trend.abs().gte(kendall90)), 1) \
         .where(lf_trend.select('scale').gt(0).And(mk_trend.abs().gte(kendall95)), 2) \
         .where(lf_trend.select('scale').gt(0).And(mk_trend.abs().gte(kendall99)), 3) \
