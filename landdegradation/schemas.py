f25cca66225fa10e67fee21d0c86e80b6cf58375
Alex Zvoleff <azvoleff@gmail.com>
2017-12-29 14:05:09 -0500

    Update schemas.

--- landdegradation/schemas.py
+++ landdegradation/schemas.py
@@ -33,20 +33,6 @@ class TimeSeriesTableSchema(Schema):
 ################################################################################
 # Schema for downloads
 
-class ComposableDict(fields.Dict):
-    '''https://github.com/marshmallow-code/marshmallow/issues/432#issuecomment-210282401'''
-
-    def __init__(self, inner, *args, **kwargs):
-        self.inner = inner
-        super(ComposableDict, self).__init__(*args, **kwargs)
-
-    def _serialize(self, value, attr, obj):
-        return {
-            key: self.inner._serialize(val, key, value)
-            for key, val in value.datasets.items()
-        }
-
-
 class Metadata(object):
     def __init__(self, band_number, no_data_value, add_to_map=False):
         self.band_number = band_number
@@ -60,9 +46,9 @@ class MetadataSchema(Schema):
     add_to_map = fields.Boolean()
 
 
-class DatasetList(object):
-    def __init__(self, datasets={}):
-        self.datasets = datasets
+class BandList(object):
+    def __init__(self, bands=[]):
+        self.bands = bands
 
 
 class URLList(object):
@@ -75,15 +61,16 @@ class URLListSchema(Schema):
     base = fields.Str()
     files = fields.List(fields.Str())
 
+
 class CloudResults(object):
-    def __init__(self, name, datasets, urls):
+    def __init__(self, name, bands, urls):
         self.type = "CloudResults"
         self.name = name
-        self.datasets = datasets
+        self.bands = bands
         self.urls = urls
 
 class CloudResultsSchema(Schema):
     type = fields.Str()
     name = fields.Str()
-    datasets = ComposableDict(fields.Nested(MetadataSchema))
+    bands = fields.List(MetadataSchema())
     urls = fields.Nested(URLListSchema())
