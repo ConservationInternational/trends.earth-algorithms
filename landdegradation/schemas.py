133bec71accc5cfe42f169bd215fef3bbb956427
Alex Zvoleff <azvoleff@gmail.com>
2018-01-09 15:48:46 -0500

    Rename title_strings to metadata and make it a dict.

--- landdegradation/schemas.py
+++ landdegradation/schemas.py
@@ -36,12 +36,12 @@ class TimeSeriesTableSchema(Schema):
 # Schema for downloads
 
 class BandInfo(object):
-    def __init__(self, name, band_number, no_data_value, add_to_map=False, title_strings=[]):
+    def __init__(self, name, band_number, no_data_value, add_to_map=False, metadata={}):
         self.name = name
         self.band_number = band_number
         self.no_data_value = no_data_value
         self.add_to_map = add_to_map
-        self.title_strings = title_strings
+        self.metadata = metadata
 
 
 class BandInfoSchema(Schema):
@@ -49,7 +49,7 @@ class BandInfoSchema(Schema):
     band_number = fields.Integer()
     no_data_value = fields.Number()
     add_to_map = fields.Boolean()
-    title_strings = fields.List(fields.Str())
+    metadata = fields.Dict()
 
 
 class URLList(object):
