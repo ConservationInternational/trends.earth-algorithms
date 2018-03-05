e4fd1330dba8197859ddbd369cc2b401efc570e7
Alex Zvoleff <azvoleff@gmail.com>
2018-03-05 08:05:22 -0500

    Fix syntax error.

--- landdegradation/download.py
+++ landdegradation/download.py
@@ -19,7 +19,7 @@ def download(asset, name, temporal_resolution, start_year=None, end_year=None,
     if temporal_resolution != "one time":
         assert (start_year and end_year), "start year or end year not defined"
         out = in_img.select('y{}'.format(start_year))
-        band_info = [BandInfo(name, metadata={'year': start_year})
+        band_info = [BandInfo(name, metadata={'year': start_year})]
         for y in range(start_year + 1, end_year + 1):
             out.addBands(in_img.select('y{}'.format(start_year)))
             band_info.append(BandInfo(name, metadata={'year': start_year}))
