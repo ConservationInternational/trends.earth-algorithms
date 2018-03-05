231d0c4507b95f2cb494086fbfdcefe28b0cc116
Alex Zvoleff <azvoleff@gmail.com>
2018-03-04 23:37:14 -0500

    Fix schemas install.

--- setup.py
+++ setup.py
@@ -71,7 +71,9 @@ setup(
     install_requires=['python-dateutil',
                       'marshmallow',
                       'earthengine-api',
-                      'git+git://github.com/conservationinternational/trends.earth.git#egg=ldmp'],
+                      'git+git://github.com/conservationinternational/trends.earth.git#egg=trendsearth-schemas&subdirectory=LDMP/schemas'],
+    
+
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
