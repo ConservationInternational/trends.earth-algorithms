fdf50b4afb168b91396fffa56e7190294dacc7d8
Alex Zvoleff <azvoleff@gmail.com>
2018-07-20 08:56:46 -0400

    Update to match latest Javascript code.

--- landdegradation/carbon.py
+++ landdegradation/carbon.py
@@ -30,6 +30,10 @@ def tc(fc_threshold, year_start, year_end, method, biomass_data, EXECUTION_ID,
     # All datasets will be reprojected to Hansen resolution
     agb = agb.reproject(crs=hansen.projection())
 
+    # JRC Global Surface Water Mapping Layers, v1.0 (>50% occurrence)
+    water = ee.Image("JRC/GSW1_0/GlobalSurfaceWater").select("occurrence")
+    water = water.reproject(crs=hansen.projection())
+
     # reclass to 1.broadleaf, 2.conifer, 3.mixed, 4.savanna
     f_type = ee.Image("users/geflanddegradation/toolbox_datasets/esa_forest_expanded_2015") \
         .remap([50,60,61,62,70,71,72,80,81,82,90,100,110],
@@ -37,7 +41,8 @@ def tc(fc_threshold, year_start, year_end, method, biomass_data, EXECUTION_ID,
     f_type = f_type.reproject(crs=hansen.projection())
 
     # IPCC climate zones reclassified as from http://eusoils.jrc.ec.europa.eu/projects/RenewableEnergy/
-    # 0-No data, 1-Warm Temperate Moist, 2-Warm Temperate Dry, 3-Cool Temperate Moist, 4-Cool Temperate Dry, 5-Polar Moist,
+    # 0-No data, 1-Warm Temperate Moist, 2-k Temperate Dry, 3-Cool Temperate 
+    # Moist, 4-Cool Temperate Dry, 5-Polar Moist,
     # 6-Polar Dry, 7-Boreal Moist, 8-Boreal Dry, 9-Tropical Montane, 10-Tropical Wet, 11-Tropical Moist, 12-Tropical Dry) to
     # 0: no data, 1:trop/sub moist, 2: trop/sub dry, 3: temperate)
     climate = ee.Image("users/geflanddegradation/toolbox_datasets/ipcc_climate_zones") \
@@ -97,16 +102,20 @@ def tc(fc_threshold, year_start, year_end, method, biomass_data, EXECUTION_ID,
 
     ##############################################/
     # define forest cover at the starting date
-    fc_str = ee.Image(1).updateMask(hansen.select('treecover2000').gte(fc_threshold)) \
-        .updateMask(hansen.select('lossyear').where(hansen.select('lossyear').eq(0), 9999).gte(year_start-2000+1))
+    fc_str = hansen.select("treecover2000").gte(tree_cover) \
+        .multiply(hansen.select('lossyear').lte(0).add(hansen.select('lossyear').gte(yr_str-2000))) \
+        .rename('fc'+(yr_str))
 
     # Create three band layer clipped to study area
     # Band 1: forest layer for initial year (0) and year loss coded as numbers (e.g. 1 = 2001)
     # Band 2: root to shoot ratio 
     # Band 3: total carbon stocks (tons of C per ha)
-    output = fc_str.multiply(hansen.select('lossyear')).unmask(-32768) \
-        .addBands(rs_ratio.multiply(100)).updateMask(fc_str.eq(1)).unmask(-32768) \
-        .addBands(tbcarbon.multiply(10)).updateMask(fc_str.eq(1)).unmask(-32768)
+    output = fc_str.multiply(hansen.select('lossyear') \
+        .lte(yr_end-2000).multiply(hansen.select('lossyear'))) \
+        .where(fc_str.eq(0),-1) \
+        .where(water.gte(50),-2).unmask(-32768) \
+        .addBands((rs_ratio.multiply(100)).multiply(fc_str)).unmask(-32768) \
+        .addBands((tbcarbon.multiply(10)).multiply(fc_str)).unmask(-32768)
     output = output.reproject(crs=hansen.projection())
 
     logger.debug("Setting up output.")
