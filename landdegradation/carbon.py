16f3499caf79f21be69a17085c37b3f09b88c2c1
Alex Zvoleff <azvoleff@gmail.com>
2018-07-20 09:18:58 -0400

    Fix variable names.

--- landdegradation/carbon.py
+++ landdegradation/carbon.py
@@ -103,15 +103,14 @@ def tc(fc_threshold, year_start, year_end, method, biomass_data, EXECUTION_ID,
     ##############################################/
     # define forest cover at the starting date
     fc_str = hansen.select("treecover2000").gte(fc_threshold) \
-        .multiply(hansen.select('lossyear').lte(0).add(hansen.select('lossyear').gte(yr_str-2000))) \
-        .rename('fc'+(yr_str))
+        .multiply(hansen.select('lossyear').lte(0).add(hansen.select('lossyear').gte(year_start-2000)))
 
     # Create three band layer clipped to study area
     # Band 1: forest layer for initial year (0) and year loss coded as numbers (e.g. 1 = 2001)
     # Band 2: root to shoot ratio 
     # Band 3: total carbon stocks (tons of C per ha)
     output = fc_str.multiply(hansen.select('lossyear') \
-        .lte(yr_end-2000).multiply(hansen.select('lossyear'))) \
+        .lte(year_end-2000).multiply(hansen.select('lossyear'))) \
         .where(fc_str.eq(0),-1) \
         .where(water.gte(50),-2).unmask(-32768) \
         .addBands((rs_ratio.multiply(100)).multiply(fc_str)).unmask(-32768) \
