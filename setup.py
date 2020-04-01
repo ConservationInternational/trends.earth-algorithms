cadb7ead4203ce13791f6cefc0a2a12d5202febf
Alex Zvoleff <azvoleff@gmail.com>
2020-03-31 20:41:19 -0400

    Support Python 3.8

--- setup.py
+++ setup.py
@@ -51,6 +51,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
 
     # What does your project relate to?
