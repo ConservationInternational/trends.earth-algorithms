673dba1f3e7b18810e6df3d90fc0f0f5456af887
Alex Zvoleff <azvoleff@gmail.com>
2020-04-20 11:17:11 -0400

    Upgrade earthengine api version.

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     # https://packaging.python.org/en/latest/requirements.html
     install_requires=['python-dateutil',
                       'marshmallow==3.3.0',
-                      'earthengine-api==0.1.213'],
+                      'earthengine-api==0.1.215'],
     
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
