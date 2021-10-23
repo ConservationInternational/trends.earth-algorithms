3c8c15fc31f49c4dc411858bccae16a57ca52d2c
Alex Zvoleff <azvoleff@conservation.org>
2021-10-23 10:21:33 -0400

    Fix setup.py

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages('te_algorithms'),
+    packages=['te_algorithms'],
     package_dir={'te_algorithms': 'te_algorithms'},
     package_data={'te_algorithms': ['version.json']},
     include_package_data=True,
