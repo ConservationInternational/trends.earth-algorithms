ba68d8ed3655fe90cb67106a57bd99ff31ff85da
Alex Zvoleff <azvoleff@conservation.org>
2022-01-19 20:15:21 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.99.1',
+    version='1.99.3',
     description=
     'A python package to facilitate analyzing remotely-sensed datasets from GEE in support of monitoring land degradation.',
     long_description=long_description,
