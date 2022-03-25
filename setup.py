ef4341d751d4ae2f8b0de3a84e649f11eee36897
Alex Zvoleff <azvoleff@conservation.org>
2022-03-25 08:49:41 -0400

    Fix version number

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.9',
+    version='1.99.9',
     description=
     'A python package to facilitate analyzing remotely-sensed datasets from GEE in support of monitoring land degradation.',
     long_description=long_description,
