0f5d6d5f184797136b8e62977f2257e565b61194
Alex Zvoleff <azvoleff@conservation.org>
2022-04-15 15:16:38 -0400

    Add function needed for yes/no

--- tasks.py
+++ tasks.py
@@ -1,15 +1,51 @@
 # -*- coding: utf-8 -*-
-
+import json
 import os
-import sys
 import re
 import shutil
 import subprocess
+import sys
+from datetime import datetime
+from datetime import timezone
 from tempfile import mkstemp
-import json
-from datetime import datetime, timezone
 
-from invoke import Collection, task
+from invoke import Collection
+from invoke import task
+
+def query_yes_no(question, default="yes"):
+    """Ask a yes/no question via input() and return answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True, "no": False, "n": False}
+
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = input().lower()
+
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write(
+                "Please respond with 'yes' or 'no' "
+                "(or 'y' or 'n').\n"
+            )
 
 
 def get_version(c):
