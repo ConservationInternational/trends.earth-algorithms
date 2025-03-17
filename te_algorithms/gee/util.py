57eae7988c4164c993e989c859665d176be28695
Alex Zvoleff <azvoleff@conservation.org>
2025-03-17 11:31:50 -0400

    Optimize

--- te_algorithms/gee/util.py
+++ te_algorithms/gee/util.py
@@ -407,8 +407,8 @@ class GEEImage:
             if outer_index in duplicates:
                 continue
             outer_band = self.bands[outer_index]
-            for inner_index in range(1, len(self.bands)):
-                if (outer_index == inner_index) or (inner_index in duplicates):
+            for inner_index in range(outer_index + 1, len(self.bands)):
+                if inner_index in duplicates:
                     continue
                 inner_band = self.bands[inner_index]
                 if all(
